318981dc8ee41f0bed9d546558a6f2cad293c013
jmgoodman <j.mike.goodman@gmail.com>
2022-03-20 15:20:52 +0100

    Delete test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-# print("Hello World")
-# a = 2+3
-# print(a)
-#
-# ughhh what a pain in the butt
-# anaconda is ok for local
-# but if I wanna deploy
-# heroku apparently requires venv to be set up here...
